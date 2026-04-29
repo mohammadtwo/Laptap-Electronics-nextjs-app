58af606e5d88e6ef7bb4ce4f40f9e4480bafe25c
mohammadtwo <bentenison61@gmail.com>
2026-04-29 11:03:25 +0330

    feat(redux): create store theme

---
+++ src/layout/global/redux-provider/redux-provider.tsx
@@ -0,0 +1,10 @@
+// components/ReduxProvider.tsx
+"use client";
+
+import { Provider } from "react-redux";
+import store from "@/redux/store/root-redux";
+import { PropsWithChildren } from "react";
+
+export default function ReduxProvider({ children }: PropsWithChildren) {
+  return <Provider store={store}>{children}</Provider>;
+}
