825d98e56032cc803d5039eb7527238a36f13c20
mohammadtwo <bentenison61@gmail.com>
2026-04-29 11:05:30 +0330

    feat (theme) create theme provider

---
+++ src/layout/global/theme/theme.tsx
@@ -0,0 +1,16 @@
+"use client"
+import { RootState } from "@/redux/store/root-redux";
+import { useSelector } from "react-redux";
+import { ReactNode } from "react";
+
+export default function ThemeProvider({ children }: { children: ReactNode }) {
+  const theme = useSelector((state: RootState) => state.theme.value);
+
+  return (
+    <div
+      className={` bg-neutral-50 text-neutral-800 ${theme === "dark" ? "dark" : ""} `}
+    >
+      {children}
+    </div>
+  );
+}
